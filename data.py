b3c55d50d3c298abd0f76fb6f9b855cad2da4a22
DAVID GOMEZ <davidgomez@MacBook-Air-de-DAVID.local>
2025-06-18 20:50:04 -0600

    Correcciones del sprint 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-a3f2ba20-5549-49e7-b9d1-68b5f45e46af.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-0c5f38c9-f92a-4dd6-b656-89845b1cadc1.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
